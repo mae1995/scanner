45c4c7cafe5a25b17135b2054b502e3fa114621d
Michael A. E <mae.1995.germany@gmail.com>
2019-09-15 16:29:07 +0200

    bugifx

--- control/Controller.py
+++ control/Controller.py
@@ -1,7 +1,19 @@
+from view.Console import Console
+from model.PortScanner import PortScanner
+
+
 class Controller:
 
     def __init__(self):
         self.isRunnig = True
 
-    def Init():
-        pass
+    def Init(self):
+        mainView = Console()
+        portScanner = PortScanner("scanme.nmap.org")
+
+        print("PortScanner is Running now...")
+
+        for port in range (1, 128):
+            result = portScanner.StartScan(port)
+            mainView.PrintBooleanRow("Port : " + str(result['port']), (result['result'] == 0), ["OPEN", "CLOSE"])
+
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from view.Console import Console
-from model.PortScanner import PortScanner
+from control.Controller import Controller
+
+control = Controller()
+control.Init()
 
 
-
-
-mainView = Console()
-
-
-portScanner = PortScanner("scanme.nmap.org")
-
-print("\cls")
-
-for port in range (1, 128):
-    result = portScanner.StartScan(port)
-    mainView.PrintBooleanRow("Port : " + str(result['port']), (result['result'] == 0), ["OPEN", "CLOSE"])
